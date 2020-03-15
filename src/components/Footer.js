050f42dbc5ae64fe8cc072761489ff9c4fbec873
Brian <brianseo1004@gmail.com>
2020-03-15 17:36:58 -0400

    First Commit of personal site.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,40 @@
 import React from 'react'
 
 class Footer extends React.Component {
-    render() {
-        return (
-            <div id="footer">
-                <div className="inner">
-                    <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
-                    </ul>
-                    <ul className="copyright">
-                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-                    </ul>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="footer">
+        <div className="inner">
+          <ul className="icons">
+            <li>
+              <a
+                href="https://twitter.com/KCSFreedom"
+                className="icon fa-twitter"
+              >
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/brian-seo-03a6b475/"
+                className="icon fa-linkedin"
+              >
+                <span className="label">Dribbble</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:brian.k.seo@seolutionangel.com"
+                className="icon fa-envelope-o"
+              >
+                <span className="label">Email</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Footer
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,51 @@ import Footer from './Footer'
 import avatar from '../assets/images/avatar.jpg'
 
 class Header extends React.Component {
-    render() {
-        return (
-            <header id="header">
-                <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
-                </div>
-                <Footer />
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header id="header">
+        <div className="inner">
+          <a href="#" className="image avatar">
+            <img src={avatar} alt="" />
+          </a>
+          <h1>
+            <strong>Brian Seo</strong>
+            <br />
+          </h1>
+          <h5>
+            Full-Stack Software Engineer | Investor
+            <br />
+          </h5>
+          <ul className="icons">
+            <li>
+              <a
+                href="https://twitter.com/KCSFreedom"
+                className="icon fa-twitter"
+              >
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/brian-seo-03a6b475/"
+                className="icon fa-linkedin"
+              >
+                <span className="label">Dribbble</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:brian.k.seo@seolutionangel.com"
+                className="icon fa-envelope-o"
+              >
+                <span className="label">Email</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    )
+  }
 }
 
 export default Header
